20b64ad8da02b1e7222ce23e389f10665c7a071f
Melody Wang <Wang99814>
2020-01-13 10:40:59 -0800

    01/13/2020 progress, basic game methods setup (cam, mouse, etc.)

--- core/src/com/mygdx/game/SimonGame.java
+++ core/src/com/mygdx/game/SimonGame.java
@@ -3,31 +3,28 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
 
 public class SimonGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	private OrthographicCamera cam;
+	private ShapeRenderer sr;
+	private Vector3 pos;
+	public void create() {
+		cam = new OrthographicCamera();
+		sr = new ShapeRenderer();
+		pos = new Vector3(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
+		cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
+	public void render() {
+		//logic
+		cam.update();
+		//drawing
+	}
+	public void dispose() {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
