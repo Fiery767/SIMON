c76ded140b5d9ec291483969be03e183368bddb2
fiery767 <melody.xinyu.wang@gmail.com>
2020-01-16 22:38:38 -0800

    buttons and flash on click finished

--- core/src/com/mygdx/game/SimonGame.java
+++ core/src/com/mygdx/game/SimonGame.java
@@ -2,177 +2,75 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.util.concurrent.TimeUnit;
 
 public class SimonGame extends ApplicationAdapter {
-	private OrthographicCamera cam;
-	private ShapeRenderer sr;
-	private Vector3 pos;
-	private Rectangle YELLOW;
-	private Rectangle BLUE;
-	private Rectangle RED;
-	private Rectangle GREEN;
-	private Button YellowButton;
+	private Stage stage;
 
 	@Override
 	public void create() {
-		cam = new OrthographicCamera();
-		sr = new ShapeRenderer();
-		pos = new Vector3(0, 0, 0);
-		cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		YELLOW = new Rectangle(0, 0,Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		YellowButton = new Button();
-		BLUE = new Rectangle(Gdx.graphics.getWidth() / 2, 0, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		RED = new Rectangle(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		GREEN = new Rectangle(0, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		reset();
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
+		Skin ySkin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+		ImageButton yellowButton = new ImageButton(ySkin);
+		yellowButton.setSize(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		yellowButton.getStyle().imageUp = new TextureRegionDrawable(new Texture(Gdx.files.internal("yellow.jpg")));
+		yellowButton.getStyle().imageDown = new TextureRegionDrawable(new Texture(Gdx.files.internal("yellowflash.png")));
+		yellowButton.setPosition(0, 0);
+		yellowButton.addListener(new ClickListener());
+		stage.addActor(yellowButton);
+		Skin bSkin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+		ImageButton blueButton = new ImageButton(bSkin);
+		blueButton.setSize(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		blueButton.getStyle().imageUp = new TextureRegionDrawable(new Texture(Gdx.files.internal("blue.jpg")));
+		blueButton.getStyle().imageDown = new TextureRegionDrawable(new Texture(Gdx.files.internal("blueflash.png")));
+		blueButton.setPosition(Gdx.graphics.getWidth() / 2, 0);
+		blueButton.addListener(new ClickListener());
+		stage.addActor(blueButton);
+		Skin rSkin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+		ImageButton redButton = new ImageButton(rSkin);
+		redButton.setSize(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		redButton.getStyle().imageUp = new TextureRegionDrawable(new Texture(Gdx.files.internal("red.jpg")));
+		redButton.getStyle().imageDown = new TextureRegionDrawable(new Texture(Gdx.files.internal("redflash.jpg")));
+		redButton.setPosition(0, Gdx.graphics.getHeight() / 2);
+		redButton.addListener(new ClickListener());
+		stage.addActor(redButton);
+		Skin gSkin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+		ImageButton greenButton = new ImageButton(gSkin);
+		greenButton.setSize(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		greenButton.getStyle().imageUp = new TextureRegionDrawable(new Texture(Gdx.files.internal("green.png")));
+		greenButton.getStyle().imageDown = new TextureRegionDrawable(new Texture(Gdx.files.internal("greenflash.png")));
+		greenButton.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		greenButton.addListener(new ClickListener());
+		stage.addActor(greenButton);
 	}
+
 	@Override
 	public void render() {
-		//drawing
-
-		//logic
-		if(Gdx.input.isTouched()) {
-			pos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			cam.update();
-		}
-		if(pos.x <= (Gdx.graphics.getWidth() / 2) && pos.y <= (Gdx.graphics.getHeight() / 2)) {
-			if(Gdx.input.isTouched()) {
-				yellowFlash();
-				cam.update();
-			}
-		}
-		/*
-		if(YELLOW.contains(pos.x, pos.y)) {
-			yellowFlash();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException ex) {
-
-				Thread.currentThread().interrupt();
-			}
-			pos.set(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0);
-		    cam.unproject(pos);
-		    reset();
-        }
-        */
-		if(BLUE.contains(pos.x, pos.y)) {
-			blueFlash();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException ex) {
-
-				Thread.currentThread().interrupt();
-			}
-			pos.set(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0);
-			cam.unproject(pos);
-			reset();
-		}
-		if(RED.contains(pos.x, pos.y)) {
-			redFlash();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException ex) {
-
-				Thread.currentThread().interrupt();
-			}
-			pos.set(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0);
-			cam.unproject(pos);
-			reset();
-		}
-		if(GREEN.contains(pos.x, pos.y)) {
-			greenFlash();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException ex) {
-
-				Thread.currentThread().interrupt();
-			}
-			pos.set(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0);
-			cam.unproject(pos);
-			reset();
-		}
-		cam.update();
-	}
-	@Override
-	public void dispose() {
-        sr.dispose();
-	}
-	//initial setup
-	private void reset() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear((GL20.GL_COLOR_BUFFER_BIT));
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.YELLOW);
-		sr.rect(0, 0,Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.setColor(Color.BLUE);
-		sr.rect(Gdx.graphics.getWidth() / 2, 0, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.setColor(Color.RED);
-		sr.rect(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.setColor(Color.GREEN);
-		sr.rect(0, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
-	}
-	//flashes
-	private void yellowFlash() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.GOLD);
-		sr.rect(0, 0,Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
-		cam.update();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException ex) {
-
-			Thread.currentThread().interrupt();
-		}
-		reset();
-	}
-	private void blueFlash() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.TEAL);
-		sr.rect(Gdx.graphics.getWidth() / 2, 0, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.BLUE);
-		sr.rect(Gdx.graphics.getWidth() / 2, 0, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
-	}
-	private void redFlash() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.PINK);
-		sr.rect(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.RED);
-		sr.rect(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
-	}
-	private void greenFlash() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.LIME);
-		sr.rect(0, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sr.begin(ShapeRenderer.ShapeType.Filled);
-		sr.setColor(Color.GREEN);
-		sr.rect(0, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		sr.end();
+		stage.act();
+		stage.draw();
 	}
 }
+
